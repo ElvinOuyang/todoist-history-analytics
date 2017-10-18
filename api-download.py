9cfd71c0000e938cd78a6afcadb2b871cab55e44
Chuanye "Elvin" Ouyang <elvin.ouyang@gmail.com>
2017-10-18 14:14:50 -0400

    complete all functions to fetch activity history from API and create dataframe
    api-download.py is a test file to examine functionality of functions.
    todoist_functions.py is the module with all the major functions I need
    to maintain online RDS

--- api-download.py
+++ api-download.py
@@ -1,39 +1,34 @@
 from todoist.api import TodoistAPI
-import pandas as pd
-# import numpy as np
 from sys import argv
+import todoist_functions as todofun
+import pandas as pd
 
-token = argv
+script, token = argv
+print('argv is {}'.format(token))
 
 # set up api engine to my developer app
 api = TodoistAPI(token)
 
 # get activity log from api
-
 act = api.activity.get(limit=100)
-
-
-def transform_act(act_dict):
-    for i in range(len(act_dict)):
-        for key, item in act_dict[i]['extra_data'].items():
-            act_dict[i][key] = item
-        del(act_dict[i]['extra_data'])
-    return act_dict
-
-act_clean = transform_act(act)
-act_df = pd.DataFrame(act_clean)
-
+act_df = todofun.transform_act(act)
+act_df = todofun.df_standardization(act_df)
+print('>>> act_df is created with shape {}'.format(act_df.shape))
+print('>>> below is a brief view of the data:')
 print(act_df.head())
 
-# TODO: Standardize the data type for each column from the activity log
-# using the documentation listed at
-# https://developer.todoist.com/sync/v7/?python#get-activity-logs
+full_fetch_until = datetime.datetime.now() - datetime.timedelta(weeks=15)
+full_df = todofun.act_fetch_all(api, until=full_fetch_until)
+print('>>> full_df is created with shape {}'.format(full_df.shape))
+print('>>> the history ranges from {} to {}'.format(
+    full_df.event_date.min().strftime('%Y-%m-%d'),
+    full_df.event_date.max().strftime('%Y-%m-%d')
+))
 
-# TODO: Device a function that runs a first time request to get all
-# activities. Something called act_fetch_all
-
-# TODO: Device a function that runs an incremental request to get new
-# activities since last sync. Something called act_fetch_new
-
-# TODO: Device a function/pipeline that stores and load pandas dataframe
-# of the activities in a csv (with standarized formats)
+full_df = todofun.df_standardization(pd.read_csv('all_history.csv'))
+update_df = todofun.act_fetch_new(api, full_df)
+print('>>> update_df is created with shape {}'.format(update_df.shape))
+print('>>> the history ranges from {} to {}'.format(
+    update_df.event_date.min().strftime('%Y-%m-%d'),
+    update_df.event_date.max().strftime('%Y-%m-%d')
+))
---
+++ todoist_functions.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+
+def transform_act(act_dict):
+    """
+    Function to Transform TodoistAPI Returned Activity Dictionary
+    -----INPUT-----
+    act_dict: the activity dictionary returned by TodoistAPI.activity.get()
+    -----OUTPUT-----
+    act_df: the standardized Pandas Dataframe for activity history, with all
+    possible variables included
+    """
+    for i in range(len(act_dict)):
+        for key, item in act_dict[i]['extra_data'].items():
+            act_dict[i][key] = item
+        del(act_dict[i]['extra_data'])
+    act_df = pd.DataFrame(act_dict)
+    vnames = ['client', 'content', 'due_date', 'event_date', 'event_type',
+              'id', 'initiator_id', 'last_content', 'last_due_date',
+              'name', 'object_id', 'object_type', 'parent_item_id',
+              'parent_project_id']
+    for vname in vnames:
+        if vname not in act_df.columns:
+            act_df[vname] = np.nan
+    return act_df
+
+
+def df_standardization(df):
+    """
+    Function to Standardize the Downloaded or Loaded Todoist Activity DataFrame
+    -----INPUT-----
+    df: activity dataframe that has been created with transform_act()
+    -----OUTPUT-----
+    df: activity Pandas Dataframe with needed dtypes for further analysis
+    """
+    df.client = df.client.astype('category')
+    df.content = df.content.astype(str)
+    df.due_date = pd.to_datetime(df.due_date)
+    df.event_date = pd.to_datetime(df.event_date)
+    df.event_type = df.event_type.astype('category')
+    df.id = df.id.astype(str)
+    df.initiator_id = df.initiator_id.astype(str)
+    df.last_content = df.last_content.astype(str)
+    df.last_due_date = pd.to_datetime(df.last_due_date)
+    df.name = df.name.astype(str)
+    df.object_id = df.object_id.astype(str)
+    df.object_type = df.object_type.astype('category')
+    df.parent_item_id = df.parent_item_id.astype(float)
+    df.parent_project_id = df.parent_project_id.astype(float)
+    return df
+
+
+def act_fetch_all(api_engine, until=datetime.datetime.now()):
+    """
+    Function to Download All Activity History as Todoist Activity
+    -----INPUT-----
+    api_engine: a TodoistAPI(token) object that connects to Todoist API
+    until: a datetime.datetime object that signifies the latest time the
+    user wants to fetch the history. Defaults to the time of the excution.
+    -----OUTPUT-----
+    act_df: a standardized Activity DataFrame with all activity history
+    -----DEPENDENCY-----
+    transform_act(): transforms dictionary to Pandas DataFrame
+    df_standardization(): standardizes Pandas DataFrame
+    """
+    until_param = until.strftime('%Y-%m-%dT%H:%M:%S')
+    act_df = df_standardization(transform_act(
+        api_engine.activity.get(limit=100, until=until_param)))
+    # print('act_df is downloaded until {}'.format(until_param))
+    new_len = len(act_df)
+    # print('new_len is {}'.format(new_len))
+    while new_len == 100:
+        until = act_df.event_date.min() - datetime.timedelta(seconds=1)
+        until_param = until.strftime('%Y-%m-%dT%H:%M:%S')
+        new_df = df_standardization(transform_act(
+            api_engine.activity.get(limit=100, until=until_param)))
+        # print('act_df is downloaded until {}'.format(until_param))
+        new_len = len(new_df)
+        # print('new_len is updated to {}'.format(new_len))
+        act_df = act_df.append(new_df)
+    return act_df
+
+
+def act_fetch_new(api_engine, act_df, until=datetime.datetime.now()):
+    """
+    Function to Update Activity History as Todoist Activity
+    -----INPUT-----
+    api_engine: a TodoistAPI(token) object that connects to Todoist API
+    act_df: the previously loaded standardized Pandas DataFrame
+    until: a datetime.datetime object that signifies the latest time the
+    user wants to fetch the history. Defaults to the time of the excution.
+    -----OUTPUT-----
+    act_df: an updated standardized full Activity DataFrame
+    df: the new activity history since last fetch
+    -----DEPENDENCY-----
+    transform_act(): transforms dictionary to Pandas DataFrame
+    df_standardization(): standardizes Pandas DataFrame
+    """
+    since = act_df.event_date.max()
+    since_param = since.strftime('%Y-%m-%dT%H:%M:%S')
+    until_param = until.strftime('%Y-%m-%dT%H:%M:%S')
+    df = df_standardization(transform_act(
+        api_engine.activity.get(limit=100, since=since_param,
+                                until=until_param)))
+    new_len = len(df)
+    while new_len == 100:
+        since = act_df.event_date.max() + datetime.timedelta(seconds=1)
+        since_param = since.strftime('%Y-%m-%dT%H:%M:%S')
+        new_df = df_standardization(transform_act(
+            api_engine.activity.get(limit=100, since=since_param,
+                                    until=until_param)))
+        new_len = len(new_df)
+        df = df.append(new_df)
+    act_df = act_df.append(df)
+    return act_df, df
+
+
+# TODO: create functions to save the full fetch dataframe to my online RDS
+
+# TODO: create functions to insert newly fecthed data to my online RDS
